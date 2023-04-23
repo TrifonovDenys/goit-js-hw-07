562acef8be51189feb37b5c1034f94b2ad8407f4
Trifonov Denys <trifonowden123@gmail.com>
2023-04-23 16:47:36 +0300

    +2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBox = document.querySelector('.gallery')
+
+const createGalleryTable = () => {
+  const galleryTable = galleryItems
+    .map(
+      ({ preview, original, description }) => 
+    `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a></li>`
+  ).join('')
+   return galleryTable;
+}
+
+galleryBox.insertAdjacentHTML('beforeend', createGalleryTable());
+
+galleryBox.addEventListener('click', galleryImgShow);
+
+var lightbox = new SimpleLightbox('.gallery a',
+  {
+    captionsData: 'alt',
+    captionDelay: 250,
+  })
+
+  
+function galleryImgShow(event) {
+  event.preventDefault();
+  lightbox
+}
