63c16ce5a2e5539e381e5d1e852e0b8c29db1848
Trifonov Denys <trifonowden123@gmail.com>
2023-04-23 16:53:08 +0300

    bux fix

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,29 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const galleryBox = document.querySelector('.gallery')
+const galleryBox = document.querySelector('.gallery');
 
 const createGalleryTable = () => {
   const galleryTable = galleryItems
     .map(
-      ({ preview, original, description }) => 
-    `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a></li>`
-  ).join('')
-   return galleryTable;
-}
+      ({ preview, original, description }) =>
+        `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a></li>`
+    )
+    .join('');
+  return galleryTable;
+};
 
 galleryBox.insertAdjacentHTML('beforeend', createGalleryTable());
 
 galleryBox.addEventListener('click', galleryImgShow);
 
-var lightbox = new SimpleLightbox('.gallery a',
-  {
-    captionsData: 'alt',
-    captionDelay: 250,
-  })
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
-  
 function galleryImgShow(event) {
   event.preventDefault();
-  lightbox
+  lightbox;
 }
