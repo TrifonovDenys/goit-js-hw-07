61bff5186cc18901ba15e3c5f8b0cfaf28dd9688
Trifonov Denys <54479637+TrifonovDenys@users.noreply.github.com>
2023-04-24 17:06:30 +0300

    undefined click

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,7 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryBox = document.querySelector('.gallery');
+const galleryBox = document.querySelector(".gallery");
 
 const createGalleryTable = () => {
   const galleryTable = galleryItems
@@ -9,26 +9,28 @@ const createGalleryTable = () => {
       ({ preview, original, description }) =>
         `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src='${preview}' data-source='${original}' alt='${description}'></a></li>`
     )
-    .join('');
+    .join("");
 
   return galleryTable;
 };
 
-galleryBox.insertAdjacentHTML('beforeend', createGalleryTable());
+galleryBox.insertAdjacentHTML("beforeend", createGalleryTable());
 
-galleryBox.addEventListener('click', galleryImgShow);
+galleryBox.addEventListener("click", galleryImgShow);
 
 function galleryImgShow(event) {
   event.preventDefault();
-
+  if (event.target.dataset.source === undefined) {
+    return;
+  }
   const instance = basicLightbox.create(
     `<img src="${event.target.dataset.source}" >`,
     {
-      onShow: instance => {
-        window.addEventListener('keydown', onEscButton);
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEscButton);
       },
-      onClose: instance => {
-        window.removeEventListener('keydown', onEscButton);
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onEscButton);
       },
     }
   );
@@ -36,7 +38,7 @@ function galleryImgShow(event) {
   instance.show();
 
   function onEscButton(event) {
-    if (event.code === 'Escape') {
+    if (event.code === "Escape") {
       console.log(event);
       instance.close();
     }
